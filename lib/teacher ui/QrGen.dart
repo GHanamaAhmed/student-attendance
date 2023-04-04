a9c3f3d5905c8048559f886f1e9d0b6f2900e784
pc <foxdeath100@gmail.com>
2023-04-04 03:49:49 +0200

    update 4/3/2023

--- lib/teacher ui/QrGen.dart
+++ lib/teacher ui/QrGen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
-
-
+import 'package:qr_flutter/qr_flutter.dart';
+import 'dart:math';
+import 'dart:convert';
 
 
 class QrGen extends StatefulWidget {
@@ -10,23 +10,46 @@ class QrGen extends StatefulWidget {
   @override
   State<QrGen> createState() => _QrGenState();
 }
-
+int x = 4;
+String qrData = "https://www.example.com";
+String generateRandomString(int length) {
+  var random = Random.secure();
+  var values = List<int>.generate(length, (i) => random.nextInt(256));
+  return base64Url.encode(values);
+}
 class _QrGenState extends State<QrGen> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: SizedBox(
-              width: 500,
-              height: 500,
-              child:Image.asset('assets/images/testqrcode.png') ,
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.9,
+          child: Column(
 
-            ),
-          )
-        ],
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              QrImage(
+                data: qrData,
+                version: QrVersions.auto,
+                size: 200.0,
+              ),
+              Container(
+                width: double.maxFinite,
+                margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 20)),
+                  onPressed: () {
+                    setState(() {
+                      qrData = "https://www.example.com/new ${generateRandomString(7)}";
+                    });
+                  },
+                  child: const Text('generate qr code'),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
